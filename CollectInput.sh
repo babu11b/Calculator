851a2bf740a48c1d15facdabedf405776acc34c5
Prathap <babu.11b@gmail.com>
2021-07-16 06:41:39 +0000

    CollectInput

--- CollectInput.sh
+++ CollectInput.sh
@@ -1,7 +1,11 @@
 Collecting Input #1, #2
+<<<<<<< Updated upstream
 
 Feature Subtract Implemented
 Hotfix Implemented
 
 Feature Subtract Implementeda    dddd
 
+=======
+Feature Subtract Implementeda    dddd
+>>>>>>> Stashed changes
