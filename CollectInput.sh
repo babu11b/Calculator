4f4e2be19f24acd0f19c7bdc21da7c82475d439c
Prathap <babu.11b@gmail.com>
2021-07-16 06:49:22 +0000

    #2 Bug fixed

--- CollectInput.sh
+++ CollectInput.sh
@@ -1,11 +1,8 @@
 Collecting Input #1, #2
-<<<<<<< Updated upstream
 
 Feature Subtract Implemented
 Hotfix Implemented
 
 Feature Subtract Implementeda    dddd
 
-=======
-Feature Subtract Implementeda    dddd
->>>>>>> Stashed changes
+Faaaaaaaaaaaaaaaeature Subtract Implementeda    dddd
